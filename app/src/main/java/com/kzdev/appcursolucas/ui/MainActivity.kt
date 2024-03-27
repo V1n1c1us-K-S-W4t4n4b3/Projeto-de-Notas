049ee41fc6143f3fa299b6d7b3d14a8019d1cef5
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-27 14:34:38 -0400

    config main activity

--- app/src/main/java/com/kzdev/appcursolucas/ui/MainActivity.kt
+++ app/src/main/java/com/kzdev/appcursolucas/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kzdev.appcursolucas.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kzdev.appcursolucas.databinding.ActivityMainBinding
@@ -11,7 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-    }
 
-    
+        binding.fab.setOnClickListener {
+            val i = Intent(this, ContentActivity::class.java)
+            startActivity(i)
+        }
+    }
 }
