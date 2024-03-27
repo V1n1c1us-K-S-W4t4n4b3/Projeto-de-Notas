297a82721fd26fae12211b2f069be710923d113b
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-27 16:24:46 -0400

    config return first activity

--- app/src/main/java/com/kzdev/appcursolucas/ui/ContentActivity.kt
+++ app/src/main/java/com/kzdev/appcursolucas/ui/ContentActivity.kt
@@ -1,14 +1,23 @@
 package com.kzdev.appcursolucas.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kzdev.appcursolucas.databinding.ActivityContentBinding
 
 class ContentActivity : AppCompatActivity() {
 
-   private val binding by lazy { ActivityContentBinding.inflate(layoutInflater) }
+    private val binding by lazy { ActivityContentBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.toolbar.setNavigationOnClickListener { finish() }
+
+        binding.btConcluded.setOnClickListener{ finish() }
     }
 }
