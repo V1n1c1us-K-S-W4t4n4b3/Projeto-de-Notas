d9074f7943af800995b4a1939d3b931a132ce4f3
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-29 18:28:06 -0400

    implementing logic set end get informations

--- app/src/main/java/com/kzdev/appcursolucas/ui/ContentActivity.kt
+++ app/src/main/java/com/kzdev/appcursolucas/ui/ContentActivity.kt
@@ -1,9 +1,15 @@
 package com.kzdev.appcursolucas.ui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.kzdev.appcursolucas.data.Note
+import com.kzdev.appcursolucas.database.AppDatabase
 import com.kzdev.appcursolucas.databinding.ActivityContentBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ContentActivity : AppCompatActivity() {
 
@@ -18,6 +24,27 @@ class ContentActivity : AppCompatActivity() {
     private fun setupListeners() {
         binding.toolbar.setNavigationOnClickListener { finish() }
 
-        binding.btConcluded.setOnClickListener{ finish() }
+        binding.btConcluded.setOnClickListener {
+            val note = Note(note = binding.tietEvNote.text.toString())
+            saveNote(note)
+        }
+    }
+
+    private fun saveNote(note: Note) {
+
+        lifecycleScope.launch {
+
+            // escolhemos o thread IO impute output entrada e saida de info para rodar em segundo plano a ação
+            withContext(Dispatchers.IO) {
+                val db = Room.databaseBuilder(
+                        applicationContext,
+                        AppDatabase::class.java, "database-note"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+              db.noteDao().insertAll(note)
+            }
+        }
     }
 }
--- app/src/main/java/com/kzdev/appcursolucas/ui/MainActivity.kt
+++ app/src/main/java/com/kzdev/appcursolucas/ui/MainActivity.kt
@@ -3,7 +3,14 @@ package com.kzdev.appcursolucas.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import com.kzdev.appcursolucas.data.Note
+import com.kzdev.appcursolucas.database.AppDatabase
 import com.kzdev.appcursolucas.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +23,34 @@ class MainActivity : AppCompatActivity() {
         binding.fab.setOnClickListener {
             val i = Intent(this, ContentActivity::class.java)
             startActivity(i)
+        }
+
+        recoverNote()
+    }
+
+    private fun recoverNote() {
+
+        lifecycleScope.launch {
+
+            val listNotes = mutableListOf<Note>()
+
+            withContext(Dispatchers.IO) {
+
+                val db =
+                    Room.databaseBuilder(
+                        applicationContext,
+                        AppDatabase::class.java,
+                        "database-note"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                listNotes.addAll(db.noteDao().getAll())
+            }
+
+            withContext(Dispatchers.Main) {
+                binding.tvNote.text = listNotes.joinToString { it.note ?: "<Sem Valor>" }
+            }
         }
     }
 }
