f04ed16dd2ddb090f9e9648c7b9f04676aae57c9
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-29 18:27:40 -0400

    update data base

--- app/src/main/java/com/kzdev/appcursolucas/database/AppDatabase.kt
+++ app/src/main/java/com/kzdev/appcursolucas/database/AppDatabase.kt
@@ -2,10 +2,10 @@ package com.kzdev.appcursolucas.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.kzdev.appcursolucas.data.Note
 import com.kzdev.appcursolucas.dao.NoteDao
+import com.kzdev.appcursolucas.data.Note
 
-@Database(entities = [Note::class], version = 1)
+@Database(entities = [Note::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
