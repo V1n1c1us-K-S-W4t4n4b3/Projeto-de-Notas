435bcbb09ca4148195c7b0db099632b8801ab5f1
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-29 18:59:46 -0400

    creating adapter

---
+++ app/src/main/java/com/kzdev/appcursolucas/adapter/AdapterNote.kt
@@ -0,0 +1,39 @@
+package com.kzdev.appcursolucas.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kzdev.appcursolucas.data.Note
+import com.kzdev.appcursolucas.databinding.ItemListNoteBinding
+
+class AdapterNote(
+    private var dataSet: List<Note>,
+) : RecyclerView.Adapter<AdapterNote.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemListNoteBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val id = dataSet[position].uid
+        val note = dataSet[position].note
+
+        viewHolder.binding.tvId.text = id.toString()
+        viewHolder.binding.tvNote.text = note
+
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    class ViewHolder(val binding: ItemListNoteBinding) : RecyclerView.ViewHolder(binding.root)
+
+    fun updateData(newList: List<Note>) {
+        dataSet = newList
+        notifyDataSetChanged()
+    }
+}
